3c6d548f8b20ff9f5ba4057c59345ae14e288948
uo282874 <uo282874@uniovi.es>
2025-03-17 09:39:19 +0100

    pairs

--- init.lua
+++ init.lua
@@ -925,6 +925,9 @@ require('lazy').setup({
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
+      -- Pairs
+      require('mini.pairs').setup()
+
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
